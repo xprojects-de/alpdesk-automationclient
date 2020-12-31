eae765ed2506b013d11b1b78ab1da0fc8d6ffbb9
XProjects B.Hummel <infos@x-projects.de>
2020-12-31 09:32:10 +0100

    delete spec.ts files

--- src/app/classes/homeautomation-categorie.spec.ts
+++
@@ -1,7 +0,0 @@
-import { HomeautomationCategorie } from './homeautomation-categorie';
-
-describe('HomeautomationCategorie', () => {
-  it('should create an instance', () => {
-    expect(new HomeautomationCategorie()).toBeTruthy();
-  });
-});
