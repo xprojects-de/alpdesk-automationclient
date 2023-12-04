76264cbd2385353e9ec2d695e8b7a0d9c29c5035
xprojects-de <infos@x-projects.de>
2023-12-04 12:21:01 +0100

    update dependencies

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -46,9 +46,3 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js';  // Included with Angular CLI.
-
-
-/***************************************************************************************************
- * APPLICATION IMPORTS
- */
-import 'document-register-element';
